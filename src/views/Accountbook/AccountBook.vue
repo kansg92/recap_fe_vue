3fe973a53d29657b16ede0707a87a9fa848734ce
kansg92 <kansg922@gmail.com>
2023-11-08 18:03:00 +0900

    기본 컴포넌트 추가

--- src/views/Accountbook/AccountBook.vue
+++ src/views/Accountbook/AccountBook.vue
@@ -1,41 +1,45 @@
 <template>
-  <main class="lg:pl-52">
-    <div class="px-4 sm:px-6 lg:px-8">
-      <h1>AccountBook</h1>
-      <div>
-        <h2> 내 가계부 리스트 </h2>
-        새가계부 추가하기~
-
-        table...
-
+  <main class="pl-52 h-screen">
+    <div class="px-4 sm:px-6 lg:px-8 h-full">
+      <div class="float-right">
+        <ListBox :selectOption="selectOption" />
       </div>
-
-
+      <h1>AccountBook</h1>
+      <div class="h-full">
+        <div class="flex flex-col justify-center items-center h-full">
+          <EmptyProj />
+          <h2>등록된 가계부가 없습니다...</h2>
+        </div>
+      </div>
     </div>
-
-
-
   </main>
 </template>
 
 <script setup lang="ts">
 import { module, sidebarContents } from "@/stores/ui";
 import { onMounted } from "vue";
+import ListBox from "@/components/ListBox.vue";
+import EmptyProj from "@/components/EmptyProject.vue"
+
 const store = module();
 const side = sidebarContents();
 
-const content = [
-  {name: "예산", href: "/", icon: "H" },
-  {name: "가계부작성", href: "/", icon: "T" },
-  {name: "달력", href: "/", icon: "W" },
-  {name: "통계", href: "/", icon: "W" },
+const selectOption = [
+  { title: "개인가게부", description: "나의 소중한 가게부", current: true },
+  { title: "공유가게부", description: "공유자:이예나", current: false },
 ];
 
+const content = [
+  { name: "예산", href: "/", icon: "H" },
+  { name: "가계부작성", href: "/", icon: "T" },
+  { name: "달력", href: "/", icon: "W" },
+  { name: "통계", href: "/", icon: "W" },
+];
 
 onMounted(() => {
   store.setSide(true);
   store.setTop(false);
-  side.setContents(content)
+  side.setContents(content);
 });
 </script>
 
--- src/views/daily_report/DailyReportView.vue
+++ src/views/daily_report/DailyReportView.vue
@@ -1,12 +1,21 @@
 <template>
-  <main class="py-10 lg:pl-52">
-    <div class="px-4 sm:px-6 lg:px-8">DailyReport</div>
+  <main class="py-10 pl-52 h-screen">
+    <div class="px-4 sm:px-6 lg:px-8 h-full">
+      <h1>DailyReport</h1>
+      <div class="h-full">
+        <div class="flex flex-col justify-center items-center h-full">
+          <EmptyProj />
+          <h2>등록된 데일리리포트가 없습니다...</h2>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
 import { module,sidebarContents } from "@/stores/ui";
 import { onMounted } from "vue";
+import EmptyProj from "@/components/EmptyProject.vue"
 
 const store = module();
 const side = sidebarContents();
