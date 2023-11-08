588e99cf530e1523bf6657211575a0da129d7bc1
kansg92 <kansg922@gmail.com>
2023-11-08 12:43:02 +0900

    파일경로 수정

--- src/views/Accountbook/AccountBook.vue
+++ src/views/Accountbook/AccountBook.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="lg:pl-60 pl-4 py-4">
-    AccountBook Page
 
-  </div>
+    <div>
+        AccountBook
+    </div>
+
+
 </template>
 
 <script setup lang="ts">
 import { module } from "@/stores/ui";
 import { onMounted } from "vue";
-
 const store = module();
 
 onMounted(() => {
